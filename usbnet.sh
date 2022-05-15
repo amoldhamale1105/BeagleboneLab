088998b8800317bb43106a47af360f6e292c29e9
Amol Dhamale <amoldhamale1105@gmail.com>
2022-05-15 14:48:56 +0530

    fixing a silly syntax error in usbnet script. Works well now

--- usbnet.sh
+++ usbnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables --table nat --append POSTROUTING --out-interface %1 -j MASQUERADE
-iptables --append FORWARD --in-interface %2 -j ACCEPT
+iptables --table nat --append POSTROUTING --out-interface $1 -j MASQUERADE
+iptables --append FORWARD --in-interface $2 -j ACCEPT
