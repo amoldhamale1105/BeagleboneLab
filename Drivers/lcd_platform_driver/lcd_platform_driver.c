e38b88e24bf52d19cea6fab2f1c8ad8f0692341f
Amol Dhamale <amoldhamale1105@gmail.com>
2022-12-24 20:37:17 +0530

    create driver boilerplate and header based on req

--- Drivers/lcd_platform_driver/lcd_platform_driver.c
+++ Drivers/lcd_platform_driver/lcd_platform_driver.c
@@ -1,1 +1,19 @@
-/*WIP. To be developed*/
+#include "lcd_platform_driver.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Amol Dhamale");
+MODULE_DESCRIPTION("Platform driver for 16x2 character LCD");
+
+module_init(lcd_driver_init);
+module_exit(lcd_driver_exit);
+
+int __init lcd_driver_init(void)
+{
+    pr_info("lcd driver loaded successfully\n");
+    return 0;
+}
+
+void __exit lcd_driver_exit(void)
+{
+    pr_info("lcd driver unloaded\n");
+}
--- Drivers/lcd_platform_driver/lcd_platform_driver.h
+++ Drivers/lcd_platform_driver/lcd_platform_driver.h
@@ -1,1 +1,51 @@
-/*WIP. To be developed*/
+#ifndef LCD_PLATFORM_DRIVER_H
+#define LCD_PLATFORM_DRIVER_H
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+#include <linux/platform_device.h>
+#include <linux/mod_devicetable.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/slab.h>
+#include <linux/mutex.h>
+
+#undef pr_fmt
+#define pr_fmt(fmt) "%s :" fmt,__func__
+
+int __init lcd_driver_init(void);
+void __exit lcd_driver_exit(void);
+
+int lcd_driver_probe(struct platform_device*);
+int lcd_driver_remove(struct platform_device*);
+
+// Attribute show and store functions
+ssize_t lcdcmd_store(struct device*, struct device_attribute*, const char*, size_t);
+
+ssize_t lcdscroll_store(struct device*, struct device_attribute*, const char*, size_t);
+
+ssize_t lcdtext_store(struct device*, struct device_attribute*, const char*, size_t);
+
+ssize_t lcdxy_show(struct device*, struct device_attribute*, char*);
+ssize_t lcdxy_store(struct device*, struct device_attribute*, const char*, size_t);
+
+//Device private data structure
+struct lcd_private_data
+{
+    struct gpio_desc* desc;
+    struct gpio_descs* desc_arr;
+    struct mutex lcd_lock;
+};
+
+//Driver private data structure
+struct drv_private_data
+{
+    struct class *class_lcd;
+    struct device *device_lcd;
+};
+
+#endif
